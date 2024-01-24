b86dbe3ba645f757de1ae6a4e93ca900a80409b4
Hugo <hugosimoescontato@gmail.com>
2024-01-24 15:54:32 -0300

    Add: Home component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World!</h1>
+    <main className="home">
+      <div className="flex gap-[10px] item-center light-border">
+        <Image src="/icons/home.svg" width={16} height={16} />
+        <p className="text-[12px]">INTRODUCE</p>
+      </div>
+      <p className="text-[78px] leading-[90px]">
+        Say Hi from <span className="text-red-600">Hugo</span>, <br />
+        Web Developer
+      </p>
+      <p className="text-zinc-500">
+        I code beautifully simple things and i love what i do.
+        <br />
+        Just simple like that!
+      </p>
     </main>
   );
 }
