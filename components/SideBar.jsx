a06182f7fd3955346b9f74f9a1054d06a9b7f3b7
Hugo <hugosimoescontato@gmail.com>
2024-01-26 16:04:21 -0300

    Redesigning SideBar Component

---
+++ components/SideBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+
+const SideBar = () => {
+  return <aside className="max-h-max w-[40px] bg-red-500"></aside>;
+};
+
+export default SideBar;
