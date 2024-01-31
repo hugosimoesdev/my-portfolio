fc2f587f7aa438f275d3f1fe49a275ecd09e39f4
Hugo <hugosimoescontato@gmail.com>
2024-01-31 15:39:43 -0300

    feat: Add project page and fix some visual bugs

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,16 +1,54 @@
 import React from "react";
 import { GanttChartSquare } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Projects = () => {
   return (
     <main className="container">
-      <div className="flex w-fit gap-3 items-center border rounded-3xl py-2 px-4">
+      <div className="flex w-fit gap-3 border rounded-3xl py-2 px-4">
         <GanttChartSquare size={16} />
         <p className="text-xs uppercase">Projects</p>
+      </div>
+      <div className="flex flex-col mt-8">
+        <div className="w-[800px] p-3 flex item-center justify-center hover:scale-105 hover:duration-300 hover:bg-zinc-700">
+          <Image src="/no-image.jpg" width={130} height={100} alt="No image" />
+          <Link href="/resume" className="flex-1 h-[100px] p-3">
+            <p className="text-lg">Demo Project 1</p>
+            <p className="text-sm mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              sit amet elementum sem, eu consectetur velit. Quisque ac ultricies
+              erat.
+            </p>
+          </Link>
+        </div>
+        <span className="mt-3 w-[800px] border border-zinc-400 opacity-30 " />
+        <div className="w-[800px] p-3 flex item-center justify-center hover:scale-105 hover:duration-300 hover:bg-zinc-700">
+          <Image src="/no-image.jpg" width={130} height={100} alt="No image" />
+          <Link href="/resume" className="flex-1 h-[100px] p-3">
+            <p className="text-lg">Demo Project 2</p>
+            <p className="text-sm mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              sit amet elementum sem, eu consectetur velit. Quisque ac ultricies
+              erat.
+            </p>
+          </Link>
+        </div>
+        <span className="mt-3 w-[800px] border border-zinc-400 opacity-30 " />
+        <div className="w-[800px] p-3 flex item-center justify-center hover:scale-105 hover:duration-300 hover:bg-zinc-700">
+          <Image src="/no-image.jpg" width={130} height={100} alt="No image" />
+          <Link href="/resume" className="flex-1 h-[100px] p-3">
+            <p className="text-lg">Demo Project 3</p>
+            <p className="text-sm mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              sit amet elementum sem, eu consectetur velit. Quisque ac ultricies
+              erat.
+            </p>
+          </Link>
+        </div>
       </div>
     </main>
   );
 };
 
 export default Projects;
-P
