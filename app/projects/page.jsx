a18830493280af9a226fb11cf0bc7f039fe157d4
Hugo <hugosimoescontato@gmail.com>
2024-01-31 12:40:13 -0300

    fix: some problems about responsiveness visual

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,118 +1,16 @@
 import React from "react";
-import Image from "next/image";
+import { GanttChartSquare } from "lucide-react";
 
 const Projects = () => {
   return (
-    <main className="flex flex-col items-center ml-[300px] max-lg:mx-20 max-lg:w-full max-lg:items-center w-full">
-      <div className="flex gap-[10px] item-center light-border">
-        <Image src="/icons/home.svg" width={16} height={16} />
-        <p className="text-[12px]">PROJECT</p>
+    <main className="container">
+      <div className="flex w-fit gap-3 items-center border rounded-3xl py-2 px-4">
+        <GanttChartSquare size={16} />
+        <p className="text-xs uppercase">Projects</p>
       </div>
-      <p className="text-[78px] leading-[90px]">PROJECT</p>
-      <p className="text-zinc-500">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus
-        <br />
-        tellus nisl, a ornare libero aliquet sed. Sed fringilla luctus est, non
-        <br />
-        venenatis mi. Ut tristique molestie mollis. In eget risus eu massa
-        <br />
-        maximus malesuada quis id odio. Vestibulum sed lorem quis metus pulvinar
-        <br />
-        molestie eu non odio. Proin ante augue, ullamcorper ac est a, egestas
-        <br />
-        iaculis orci. Ut posuere nisl non odio elementum, eget aliquam diam
-        <br />
-        tristique. Mauris id enim magna. In consequat ipsum augue, eget
-        <br />
-        tincidunt mauris gravida ut.
-      </p>
-      <p className="text-zinc-500">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus
-        <br />
-        tellus nisl, a ornare libero aliquet sed. Sed fringilla luctus est, non
-        <br />
-        venenatis mi. Ut tristique molestie mollis. In eget risus eu massa
-        <br />
-        maximus malesuada quis id odio. Vestibulum sed lorem quis metus pulvinar
-        <br />
-        molestie eu non odio. Proin ante augue, ullamcorper ac est a, egestas
-        <br />
-        iaculis orci. Ut posuere nisl non odio elementum, eget aliquam diam
-        <br />
-        tristique. Mauris id enim magna. In consequat ipsum augue, eget
-        <br />
-        tincidunt mauris gravida ut.
-      </p>
-      <p className="text-zinc-500">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus
-        <br />
-        tellus nisl, a ornare libero aliquet sed. Sed fringilla luctus est, non
-        <br />
-        venenatis mi. Ut tristique molestie mollis. In eget risus eu massa
-        <br />
-        maximus malesuada quis id odio. Vestibulum sed lorem quis metus pulvinar
-        <br />
-        molestie eu non odio. Proin ante augue, ullamcorper ac est a, egestas
-        <br />
-        iaculis orci. Ut posuere nisl non odio elementum, eget aliquam diam
-        <br />
-        tristique. Mauris id enim magna. In consequat ipsum augue, eget
-        <br />
-        tincidunt mauris gravida ut.
-      </p>
-      <p className="text-zinc-500">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus
-        <br />
-        tellus nisl, a ornare libero aliquet sed. Sed fringilla luctus est, non
-        <br />
-        venenatis mi. Ut tristique molestie mollis. In eget risus eu massa
-        <br />
-        maximus malesuada quis id odio. Vestibulum sed lorem quis metus pulvinar
-        <br />
-        molestie eu non odio. Proin ante augue, ullamcorper ac est a, egestas
-        <br />
-        iaculis orci. Ut posuere nisl non odio elementum, eget aliquam diam
-        <br />
-        tristique. Mauris id enim magna. In consequat ipsum augue, eget
-        <br />
-        tincidunt mauris gravida ut.
-      </p>
-      <p className="text-zinc-500">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus
-        <br />
-        tellus nisl, a ornare libero aliquet sed. Sed fringilla luctus est, non
-        <br />
-        venenatis mi. Ut tristique molestie mollis. In eget risus eu massa
-        <br />
-        maximus malesuada quis id odio. Vestibulum sed lorem quis metus pulvinar
-        <br />
-        molestie eu non odio. Proin ante augue, ullamcorper ac est a, egestas
-        <br />
-        iaculis orci. Ut posuere nisl non odio elementum, eget aliquam diam
-        <br />
-        tristique. Mauris id enim magna. In consequat ipsum augue, eget
-        <br />
-        tincidunt mauris gravida ut.
-      </p>
-      <p className="text-zinc-500">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus
-        <br />
-        tellus nisl, a ornare libero aliquet sed. Sed fringilla luctus est, non
-        <br />
-        venenatis mi. Ut tristique molestie mollis. In eget risus eu massa
-        <br />
-        maximus malesuada quis id odio. Vestibulum sed lorem quis metus pulvinar
-        <br />
-        molestie eu non odio. Proin ante augue, ullamcorper ac est a, egestas
-        <br />
-        iaculis orci. Ut posuere nisl non odio elementum, eget aliquam diam
-        <br />
-        tristique. Mauris id enim magna. In consequat ipsum augue, eget
-        <br />
-        tincidunt mauris gravida ut.
-      </p>
     </main>
   );
 };
 
 export default Projects;
+P
